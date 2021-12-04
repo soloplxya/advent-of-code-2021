39744fb42f990c4b0b11dfc5bb5f2b09e0b47eec
soloplxya <69782911+soloplxya@users.noreply.github.com>
2021-12-04 16:01:24 +0800

    exercise-3 part 2

--- exercise-3.py
+++ exercise-3.py
@@ -21,7 +21,8 @@ read_file("input-exercise3.txt")
 for i in range(12):
     # initialize ones and zeroes 
     ones = 0
-    zeroes = 0 
+    zeroes = 0
+    print(len(lines))
     for j in range(len(lines)): 
         if lines[j][i] == "1": 
             ones = ones + 1 
@@ -36,12 +37,88 @@ for i in range(12):
 
 # PART 2 
 # determine oxygen generator rating 
+ogr = lines 
+j = 0
+while (len(ogr) > 1):
+    # iterate through the rows
+    ones = 0
+    zeroes = 0
+    new_array = []
+
+    for i in range(len(ogr)): 
+        print(i)
+        # print(ogr[i])
+        if ogr[i][j] == "1": 
+            ones = ones + 1 
+        elif ogr[i][j] == "0": 
+            zeroes = zeroes + 1 
+    
+
+    # if ones greater than zeroes or number of ones equals to zero
+    if (ones > zeroes) or (ones == zeroes): 
+        #print('a')
+        for i in range(len(ogr)): 
+            if ogr[i][j] == "1": 
+                new_array.append(ogr[i])
+    else: 
+        #print('b')
+        for i in range(len(ogr)): 
+            if ogr[i][j] == "0": 
+                new_array.append(ogr[i])
+    ogr = new_array
+    j = j + 1 
+
+    print(ogr)
+
+   
+
+
+
+# csr 
+csr = lines 
+j = 0
+while (len(csr) > 1):
+    # iterate through the rows
+    ones = 0
+    zeroes = 0
+    new_array = []
+
+    for i in range(len(csr)): 
+        print(i)
+        # print(ogr[i])
+        if csr[i][j] == "1": 
+            ones = ones + 1 
+        elif csr[i][j] == "0": 
+            zeroes = zeroes + 1 
+    
+
+    # if ones greater than zeroes or number of ones equals to zero
+    if (ones > zeroes) or (ones == zeroes): 
+        #print('a')
+        for i in range(len(csr)): 
+            if csr[i][j] == "0": 
+                new_array.append(csr[i])
+    else: 
+        #print('b')
+        for i in range(len(csr)): 
+            if csr[i][j] == "1": 
+                new_array.append(csr[i])
+    csr = new_array
+    j = j + 1 
+
+   
+
+        
+
 
 
     
        
 
 # RESULTS 
+print(ogr)
+print(csr)
+   
 print(gamma)
 print(epsilon)
 
