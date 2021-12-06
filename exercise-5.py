aa5289d58189010962b08741c81d3b8ba6444497
soloplxya <69782911+soloplxya@users.noreply.github.com>
2021-12-06 23:55:48 +0800

    exercise-5 part 2 (not working yet)

--- exercise-5.py
+++ exercise-5.py
@@ -50,6 +50,14 @@ def get_XY(line):
     return (start_X, start_Y, end_X, end_Y)
 
 
+def is_diagonal(start_X, start_Y, end_X, end_Y): 
+    if start_X == start_Y & end_X == end_Y:
+        return True 
+    elif start_X == end_Y & start_Y == end_X: 
+        return True
+    else: 
+        return False
+
 # check if the line is horizontal 
 def is_horizontal(start_X, start_Y, end_X, end_Y): 
     return start_X == end_X
@@ -108,6 +116,29 @@ for i in range(len(lines)):
                     two_dimen_arr[start_X - j][start_Y] += 1
                 else: 
                     two_dimen_arr[start_X + j][start_Y] += 1
+        
+        if is_diagonal(start_X, start_Y, end_X, end_Y): 
+            # case 1 the centre diagonal 
+            if start_X == start_Y & end_X == end_Y:
+                difference = get_difference(start_X, end_X)
+                for j in range(difference + 1): 
+                    if start_X > end_X: 
+                        two_dimen_arr[start_X - j][start_Y - j] += 1
+                    else: 
+                        two_dimen_arr[start_X + j][start_Y + j] += 1
+            # case 2 not centre diagonal 
+            else: 
+                difference = get_difference(start_X, start_Y)
+                for j in range(difference + 1): 
+                    if start_X > start_Y: 
+                        two_dimen_arr[start_X - j][start_Y + j] += 1
+                    else: 
+                        two_dimen_arr[start_X + j][start_Y - j] += 1
+
+
+
+
+                
                     
 
 # results 
